07b6d85b3231b2682e86d19b8bebeed81d826df8
Jonathan <jhonyramos46@gmail.com>
2024-02-23 16:42:30 -0300

    feat: adiciona classe PessoaCandidata com Herença

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,10 +1,34 @@
 package com.betrybe.sistemadevotacao;
 
 /**
- * Description:.
- * Author: ${USER}.
- * Date: ${DATE}.
+ * Description: Classe responsavel pelo voto.
  */
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos;
 
+  /**
+   * Construtor com valores padrão.
+   */
+  public PessoaCandidata(String nome, int numero) {
+    this.setNome(nome);
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
